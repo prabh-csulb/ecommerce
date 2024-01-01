873829cb251d40a243b90ee5c2f0f6efa8c18bd8
Prabhjot Singh <prabh.csulb@gmail.com>
2023-12-31 21:48:47 -0800

    Installed Redux Toolkit and React Redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import "./index.css";
+import rootReducer from "./store/index.ts";
+import { configureStore } from "@reduxjs/toolkit";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const store = configureStore(rootReducer);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-)
+  </Provider>
+);
