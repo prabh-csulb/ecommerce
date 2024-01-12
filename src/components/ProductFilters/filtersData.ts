7e5722ef0603b363bd170b2d13f192479159334b
Prabhjot Singh <prabh.csulb@gmail.com>
2024-01-11 21:00:21 -0800

    Added Filter Buttons for Products  and their Styles

---
+++ src/components/ProductFilters/filtersData.ts
@@ -0,0 +1,33 @@
+export type filterCategoriesTypes = {
+  id: string;
+  label: string;
+  filters: (number | string)[];
+};
+
+export const filterCategories: filterCategoriesTypes[] = [
+  {
+    id: "Size",
+    label: "Size",
+    filters: ["extra-small", "small", "medium", "large", "extra-large"],
+  },
+  {
+    id: "Color",
+    label: "Color",
+    filters: ["red", "black", "blue", "darkred", "brown"],
+  },
+  {
+    id: "Price",
+    label: "Price",
+    filters: ["0 < 100", "100 < 200", "200 < 300", "300 < 400", "400 < 500"],
+  },
+  {
+    id: "Model",
+    label: "Model",
+    filters: [2015, 2017, 2018, 2020, 2023],
+  },
+  {
+    id: "Discount",
+    label: "Discount",
+    filters: [10, 20, 30, 40, 50],
+  },
+];
